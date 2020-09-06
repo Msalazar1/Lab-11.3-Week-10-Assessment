37d7a4597810e07d2b568a317afe95af5691747c
Manny <mannysalazar@quickenloans.com>
2020-09-05 23:47:08 -0400

    11.3 AND Week 10 Assessment

---
+++ Lab 11.2 Coffee Shop Registration/Controllers/HomeController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Lab_11._2_Coffee_Shop_Registration.Models;
+
+namespace Lab_11._2_Coffee_Shop_Registration.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "The HOTNESS of Cool Beans Coffee.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Let us know how we're doing!";
+
+            return View();
+        }
+        public ActionResult Registration()
+        {
+            ViewBag.Message = "Register Here.";
+
+            return View();
+        }
+        public ActionResult Menu()
+        {
+            ViewBag.Message = "Order Here.";
+
+            return View();
+        }
+        public ActionResult ShoppingCart()
+        {
+            ViewBag.Message = "View Your Order Here.";
+
+            return View();
+        }
+        public ActionResult Save(string username, string firstname, string lastname, string emailaddress, string phonenumber, string birthday, string state, string password, string color)
+        {
+            WebUser user = new WebUser()
+            {
+                UserName = username,
+                FirstName = firstname,
+                LastName = lastname,
+                Email = emailaddress,
+                PhoneNumber = phonenumber,
+                BDay = birthday,
+                State = state,
+                Password = password,
+                FavoriteColor = color
+            };
+            return View(user);
+        }
+    }
+}
---
+++ Lab 11.2 Coffee Shop Registration/Models/WebUser.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lab_11._2_Coffee_Shop_Registration.Models
+{
+    public class WebUser
+    {
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string BDay { get; set; }
+        public string State { get; set; }
+        public string FavoriteColor { get; set; }
+        public string PhoneNumber { get; set; }
+        
+
+    }
+}
